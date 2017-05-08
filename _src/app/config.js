613ec5c592ad67de847ee2a80cb3a2d7ab296701
Scott Davis <stdavis@utah.gov>
2017-05-08 14:09:00 -0600

    fix config path for cors servers

--- _src/app/config.js
+++ _src/app/config.js
@@ -12,10 +12,10 @@ define([
 ) {
     // force api to use CORS on mapserv thus removing the test request on app load
     // e.g. http://mapserv.utah.gov/ArcGIS/rest/info?f=json
-    esriConfig.defaults.io.corsEnabledServers.push('mapserv.utah.gov');
-    esriConfig.defaults.io.corsEnabledServers.push('basemaps.utah.gov');
-    esriConfig.defaults.io.corsEnabledServers.push('api.mapserv.utah.gov');
-    esriConfig.defaults.io.corsEnabledServers.push('discover.agrc.utah.gov');
+    esriConfig.request.corsEnabledServers.push('mapserv.utah.gov');
+    esriConfig.request.corsEnabledServers.push('basemaps.utah.gov');
+    esriConfig.request.corsEnabledServers.push('api.mapserv.utah.gov');
+    esriConfig.request.corsEnabledServers.push('discover.agrc.utah.gov');
 
     window.AGRC = {
         // errorLogger: ijit.modules.ErrorLogger
